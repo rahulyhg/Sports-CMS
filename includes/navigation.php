83e3313391a50fc90765cc440a1dfb026c608e87
harry <3286.harinder>
2019-04-11 16:32:19 +1000

    created the nav bar and the css related to it

---
+++ includes/navigation.php
@@ -0,0 +1,16 @@
+    <header>
+    <div class="container">
+        <img src ="LOGO.png" alt= "logo" class="logo"height="70"/>
+        <nav>
+        <ul>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">RANKINGs</a></li>
+            <li><a href="#">CLUB</a></li>
+            <li><a href="#">ABOUT US</a></li>
+            <li><a href="#">SIGN UP</a></li>
+            
+            </ul>
+        </nav>
+        </div>
+    </header>
+   
