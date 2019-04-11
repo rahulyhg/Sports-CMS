63bc4fd43b8cdc7add204c80929e71fb6d9c108f
Grant Upson <gupson@utas.edu.au>
2019-04-11 16:50:59 +1000

    Quality Control
    Making the code fix with the code guidelines harry submitted

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,16 +1,15 @@
-    <header>
-    <div class="container">
-        <img src ="LOGO.png" alt= "logo" class="logo"height="70"/>
-        <nav>
-        <ul>
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">RANKINGs</a></li>
-            <li><a href="#">CLUB</a></li>
-            <li><a href="#">ABOUT US</a></li>
-            <li><a href="#">SIGN UP</a></li>
-            
-            </ul>
-        </nav>
-        </div>
-    </header>
+<header>
+  <div class="container">
+    <img src ="LOGO.png" alt= "logo" class="logo"height="70"/>
+    <nav>
+      <ul>
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">RATINGS</a></li>
+        <li><a href="#">CLUB</a></li>
+        <li><a href="#">ABOUT US</a></li>
+        <li><a href="#">SIGN UP</a></li>
+      </ul>
+   </nav>
+ </div>
+</header>
    
--- privacy.php
+++ privacy.php
@@ -1,3 +1,11 @@
 <?php
+  $title = "Peters Ratings | Privacy";
+  include("./includes/header.php");
+  include("./includes/navigation.php");
+?>
 
+
+
+<?php
+  include("./includes/footer.php");
 ?>
