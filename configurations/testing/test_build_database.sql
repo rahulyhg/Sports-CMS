66953bcfca418c63a3e954c5ecf4ac454d88970b
Grant Upson <gupson@utas.edu.au>
2019-05-12 19:56:25 +1000

    quick fix for db test build

--- configurations/testing/test_build_database.sql
+++ configurations/testing/test_build_database.sql
@@ -156,7 +156,7 @@ insert into `sport` (name) VALUES ('Table tennis');
 insert into `club` (name, country_id, state_id) VALUES ('Launceston Squash Club', 1, 1);
 insert into `club` (name, country_id, state_Id) VALUES ('Otago Badminton Club', 2, 4);
 
-insert into `player` (given_name, family_name, gender, date_of_birth, email, last_played, receive_emails, country_id) 
+insert into `player` (given_name, family_name, gender, date_of_birth, email, last_played, receive_emails, country_id, state_id) 
   VALUES ('Grant', 'Upson', 'M', NOW(), 'grantaupson@gmail.com', NOW(), 'Y', '1', '1');
 insert into `rating` (mean, standard_deviation, last_calculated, sport_id, player_id, team_id) VALUES (1500, 200, NOW(), 2, 1, null);
 
